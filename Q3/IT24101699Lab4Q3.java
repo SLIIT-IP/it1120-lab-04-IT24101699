d37be0cf778b1171a85d2a182124b8e8eb129fc3
IT24101699 <it24101699@my.sliit.lk>
2024-08-17 16:53:30 +0530

    Add files via upload

---
+++ Q3/IT24101699Lab4Q3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IT24101699Lab4Q3 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+       
+        System.out.print("Enter a number:"); 
+        int number = scanner.nextInt();
+        String message;
+
+        message = (number > 0) ? "Positive": (number<0) ? "Negative": "Zero" ;
+        System.out.print(" The number is:"+message);
+       scanner.close();
+
+    }
+}
+
+       
